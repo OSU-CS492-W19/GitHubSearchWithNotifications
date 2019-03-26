e3791a0fe692fe6eea5332e031ac83268565c74e
Rob Hess <robwhess@gmail.com>
2019-03-26 13:32:23 -0700

    Control background syncing with user preference.

--- app/src/main/java/com/example/android/githubsearch/App.java
+++ app/src/main/java/com/example/android/githubsearch/App.java
@@ -3,7 +3,10 @@ package com.example.android.githubsearch;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 
 import com.example.android.githubsearch.workers.CheckRepoStarsWorker;
 
@@ -15,15 +18,20 @@ import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
-public class App extends Application {
+public class App extends Application implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final String CHECK_STARS_WORK_NAME = "checkStars";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        startCheckStarsWorker();
         createStarsNotificationChannel();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getBoolean(getString(R.string.pref_sync_key), true)) {
+            startCheckStarsWorker();
+        }
+        preferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     private void startCheckStarsWorker() {
@@ -41,6 +49,10 @@ public class App extends Application {
                         ExistingPeriodicWorkPolicy.KEEP, workRequest);
     }
 
+    private void stopCheckStarsWorker() {
+        WorkManager.getInstance().cancelUniqueWork(CHECK_STARS_WORK_NAME);
+    }
+
     private void createStarsNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
@@ -50,6 +62,17 @@ public class App extends Application {
             );
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String s) {
+        if (s.equals(getString(R.string.pref_sync_key))) {
+            if (prefs.getBoolean(getString(R.string.pref_sync_key), true)) {
+                startCheckStarsWorker();
+            } else {
+                stopCheckStarsWorker();
+            }
         }
     }
 }
